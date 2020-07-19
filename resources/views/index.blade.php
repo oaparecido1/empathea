b88fa5e230f0ac15f6eae0453b2ae849c41f4ee6
Aparecido <aparecido@pop-os.localdomain>
2020-07-19 00:45:30 -0300

    initial config

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,7 @@
-<?php
+@extends('layouts.index')
+
+@section('content')
+    <div class="empathea_index flex-center">
+        <h1>Hello empathea</h1>
+    </div>
+@endsection
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,7 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <!-- CSRRF Token--->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('main.name', 'Empathea') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div class="container">
