47cc6767d7638a293fd14becfa8d7b085e4ffda3
Aparecido <aparecido@pop-os.localdomain>
2020-07-19 14:52:46 -0300

    modal about

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -22,7 +22,127 @@
                 <h1>Name cause</h1>
                 <h3>Location</h3>
                 <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
+                <a class="d-flex justify-content-center"data-toggle="modal" data-target="#exampleModalAssign"  href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center"  data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
+            </div>
+        </div>
+
+        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
+            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
+            <div class="mysite_home-card-content ml-3">
+                <h1>Name cause</h1>
+                <h3>Location</h3>
+                <p>Description Description Description Description Description</p>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
             </div>
         </div>
 
@@ -42,7 +162,7 @@
                 <h1>Name cause</h1>
                 <h3>Location</h3>
                 <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
             </div>
         </div>
 
@@ -52,7 +172,7 @@
                 <h1>Name cause</h1>
                 <h3>Location</h3>
                 <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
             </div>
         </div>
 
@@ -62,130 +182,22 @@
                 <h1>Name cause</h1>
                 <h3>Location</h3>
                 <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
-            </div>
-        </div>
-
-        <div class="mysite_home-card d-flex flex-row justify-content-center align-items-center p-2 m-2">
-            <img class="pr-2" src="{!! asset('img/no-racism1.png') !!}" alt="">
-            <div class="mysite_home-card-content ml-3">
-                <h1>Name cause</h1>
-                <h3>Location</h3>
-                <p>Description Description Description Description Description</p>
-                <a class="d-flex justify-content-center" href="">Assign cause</a>
+                <a class="d-flex justify-content-center" data-toggle="modal" data-target="#exampleModalAssign" href="">Assign cause</a>
             </div>
         </div>
     </div>
 
-
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalAssign" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+        <div class="modal-dialog" role="document" style="width: 30vw;">
+            <div class="modal-content" style="border: none; background: #1c7430; color: #ffffff">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Congrats, to assign!!</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" style="color: #ffffff">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
